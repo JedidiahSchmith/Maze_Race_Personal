b77442119cdfc04845c2546cd330182a86d2773d
Jedidiah Schmith <jedidiahschmith@gmail.com>
2021-04-12 19:15:25 -0600

    Changed to work in eclipse

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
-package tempPackage;
+package main;
 
-public class  Main{
-	public static void main(String[] args){
+public class Main {
+
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
+
 }
