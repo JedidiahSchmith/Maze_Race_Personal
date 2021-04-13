74c392e7b2c32504459023293505b301ec71c77b
Jedidiah Schmith <jedidiahschmith@gmail.com>
2021-04-12 19:03:12 -0600

    Fixed java class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
-package tempPackage
+package tempPackage;
 
-public static void main(String[] nil){
-	System.out.println("Hello world!");
+public class  Main{
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
 }
