ac13ffba7810c6458403256bdeadc4e5ed8f49aa
Jedidiah Schmith <jedidiahschmith@gmail.com>
2021-04-12 19:37:53 -0600

    Made temp code

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello world Number 2!");
-		
+		System.out.println(TempClass.random());
 	}
 
 }
---
+++ src/main/TempClass.java
@@ -0,0 +1,7 @@
+package main;
+
+public class TempClass {
+	public static int random() {
+		return 4;
+	}
+}
