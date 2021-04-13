d65117264693ccbfb6144d738163ddef725cf773
Jedidiah Schmith <jedidiahschmith@gmail.com>
2021-04-13 10:24:16 -0600

    Specified size

--- src/mazeGame/MazeGame.java
+++ src/mazeGame/MazeGame.java
@@ -2,7 +2,9 @@ package mazeGame;
 
 public class MazeGame {
 	public static void main(String[] nil) {
-		int size = 5;
+		int width = 3;
+		int size = width * width;
+
 		Maze maze = new Maze(size);
 
 		System.out.println(maze.mazeHolder.toString());
